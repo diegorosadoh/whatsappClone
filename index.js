fa1fdf1601f6a14261fb193e723187b8304a2b27
diegorosadoh <diegorosadoh@gmail.com>
2022-03-07 08:48:00 +0100

    “whatsapp”

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 const http = require('http');
 const server = http.createServer(app);
